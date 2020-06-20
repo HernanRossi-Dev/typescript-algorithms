3cf5b7d12c22fc7a1eaa814ce55b0a9963e226e6
HernanRossi <hernan_rossi@msn.com>
2020-06-20 13:40:44 -0700

    Interger to roman numerals

---
+++ src/RomanNumerals.ts
@@ -0,0 +1,46 @@
+function intToRoman(num: number): string {
+  if(!num) return ''
+  let result = []
+  const ones = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+  const tens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+  const hunds = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+  const thous = ['M', 'MM', 'MMM']
+
+
+  let one = num % 10
+  if(one) {
+    result.unshift(ones[one -1])
+  }
+  num = Math.floor(num / 10)
+  let ten = num % 10
+  if(ten) {
+    result.unshift(tens[ten -1])
+  }
+  num = Math.floor(num / 10)
+  let hund = num % 10
+  if(hund) {
+    result.unshift(hunds[hund-1])
+  }
+  num = Math.floor(num / 10)
+  let thou = num % 10
+  if(thou) {
+    result.unshift(thous[thou-1])
+  }
+  return result.join('')
+};
+
+
+const runTests = () => {
+  const testCases = [
+    { input: 58, expected: "LVIII" },
+    { input: 1999, expected: "MCMXCIX" },
+    { input: 2000, expected: "MM" },
+  ]
+
+  testCases.forEach(({ input, expected }) => {
+    const result = intToRoman(input)
+    console.log("Result: ", result, " expected: ", expected)
+  })
+}
+
+runTests()
